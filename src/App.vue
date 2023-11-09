35f86a7b97c49e1ab6e836c75723798ecac8f6e6
cromega <cr.jrg08@gmail.com>
2023-11-09 17:54:28 -0500

    feat: header completed and functional

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
 <script>
+import Header from "/src/components/layout/Header.vue"
 import Main from "/src/components/layout/Main"
-import Footer from '/src/components/layout/Footer.vue';
+import Footer from '/src/components/layout/Footer.vue'
+import sections from "/src/utils/data/sections.json"
 
 export default {
 	components: {
+		Header,
 		Main,
 		Footer
 	},
 	data() {
 		return {
-			dialogOpen: false
+			dialogOpen: false,
+			"sections": sections,
+			sectionsTitles: [],
+			currentSection: "Who am I?"
 		}
 	},
 	methods: {
-		showDialog(value) {
-			this.dialogOpen = value
-		}
+		showDialog(value) { this.dialogOpen = value },
+		selectSection(section) { this.currentSection = section }
+	},
+	created() {
+		this.sectionsTitles = sections.map(section => section.title)
 	}
 }
 </script>
 
 
 <template>
-  <v-app>
-    <v-main>
-		<Main :open-dialog="dialogOpen" @openDialog="value => showDialog(value)"/>
-		<Footer @openDialog="value => showDialog(value)"/>
-    </v-main>
-  </v-app>
+	<v-app>
+	    <v-main class="h-100 w-100 overflow-hidden mx-auto d-flex flex-column">
+			<Header :current-section="currentSection" :titles-data="sectionsTitles" @selectSection="section => selectSection(section)"/>
+			<Main :sections-data="sections" :open-dialog="dialogOpen" @openDialog="value => showDialog(value)"/>
+			<Footer @openDialog="value => showDialog(value)"/>
+	    </v-main>
+	</v-app>
 </template>
+
+<style>
+/* * {outline: solid red;} */
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,52 @@
+<script>
+import Icon from '/src/components/elements/Icon.vue';
+
+export default {
+	components: {
+		Icon
+	},
+	props: {
+		titlesData: JSON,
+		currentSection: String
+	},
+	data() {
+		return {
+			logo: "/src/assets/logo.svg",
+			titles: []
+		}
+	},
+	emits: ["select-section"],
+	methods: {
+		isCurrent(title) {
+			const current = this.currentSection === title
+
+			return current ? "tonal" : "text"
+		},
+		selectSection(section) {
+			this.$emit("select-section", section)
+		}
+	},
+	created() {
+		this.titles = this.titlesData
+	}
+}
+</script>
+
+<template>
+	<header class="px-7 py-5 d-flex">
+		<Icon class="mx-5" :svgPath="logo"/>
+		<h1 class="text-h4">Cromega</h1>
+		<v-spacer></v-spacer>
+		<div class="d-flex align-center">
+			<v-btn class="px-4 h-100" v-for="title in titles" :variant="isCurrent(title)" value="title" @click="selectSection(title)">{{ title }}</v-btn>
+		</div>
+	</header>
+</template>
+
+<style scoped>
+h1 {
+	display: flex;
+	align-items: center;
+}
+
+</style>
