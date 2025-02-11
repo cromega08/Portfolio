ff2164583dd0a744c5fdee64900f071d6861611f
Cromega08 <cr.jrg08@gmail.com>
2025-02-11 14:04:12 -0500

    fix: catch error in request for "GithubApi.getRepositories"
    fix: catch error in request for "GithubApi.getRepositories", alerting about the problem and returning default value

--- src/main/kotlin/data/remote/instances/GithubApi.kt
+++ src/main/kotlin/data/remote/instances/GithubApi.kt
@@ -2,21 +2,30 @@ package data.remote.instances
 
 import data.models.Repository
 import data.remote.generic.ApiRequestInterface
+import kotlinx.browser.window
 import kotlin.js.json
 
 class GithubApi : ApiRequestInterface()
 {
     override val domain: String = "https://api.github.com"
 
-    suspend fun getRepositories(): Array<Repository> =
-        toModel<Array<Repository>>(
-            response = get(
-                url = "$domain/users/cromega08/repos?apiVersion=2022-11-28&sort=created&direction=desc&per_page=100",
-                headers = json(
-                    "Content-Type" to "application/json",
-                    "Accept" to "application/json"
-                ),
-                errorText = "Unable to get projects information"
+    suspend fun getRepositories(): Array<Repository>
+    {
+        val errorText: String = "Unable to get projects information"
+        return runCatching {
+            toModel<Array<Repository>>(
+                response = get(
+                    url = "$domain/users/cromega08/repos?apiVersion=2022-11-28&sort=created&direction=desc&per_page=100",
+                    headers = json(
+                        "Content-Type" to "application/json",
+                        "Accept" to "application/json"
+                    ),
+                    errorText = errorText
+                )
             )
-        )
+        }.getOrElse {
+            window.alert(errorText)
+            arrayOf()
+        }
+    }
 }
