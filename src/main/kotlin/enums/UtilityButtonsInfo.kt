95b79730e23553667c624283ca898d308325226f
Cromega08 <cr.jrg08@gmail.com>
2025-02-11 21:03:31 -0500

    fix: fix "UtilityButtonsInfo.GITHUB" href
    fix: fix "UtilityButtonsInfo.GITHUB" href, pointing to the real Github link

--- src/main/kotlin/enums/UtilityButtonsInfo.kt
+++ src/main/kotlin/enums/UtilityButtonsInfo.kt
@@ -6,7 +6,7 @@ enum class UtilityButtonsInfo(
     val href: String
 ) {
     LINKEDIN(text = "CJRG LinkedIn", icon = "icons\\linkedin.svg", href = "https://www.linkedin.com/in/crjrg/"),
-    GITHUB(text = "Cromega Github", icon = "icons\\github.svg", href = "https://www.linkedin.com/in/crjrg/"),
+    GITHUB(text = "Cromega Github", icon = "icons\\github.svg", href = "https://github.com/Cromega08"),
     EMAIL(text = "cr.jrg08@gmail.com e-mail", icon = "icons\\email.svg", href = "mailto:cr.jrg08@gmail.com"),
     CV(text = "Cristian CV", icon = "icons\\cv.svg", href = "documents\\cv_cjrg.pdf"),
     CREDITS(text = "Resources Credits", icon = "icons\\credits.svg", href = "documents\\credits.md"),
