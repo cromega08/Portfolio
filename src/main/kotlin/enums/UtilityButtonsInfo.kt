59da95d3338752ed1fc49640cd82d9ddadb9439e
Cromega08 <cr.jrg08@gmail.com>
2025-02-12 21:58:56 -0500

    fix: update CV to include both languages

--- src/main/kotlin/enums/UtilityButtonsInfo.kt
+++ src/main/kotlin/enums/UtilityButtonsInfo.kt
@@ -8,7 +8,7 @@ enum class UtilityButtonsInfo(
     LINKEDIN(text = "CJRG LinkedIn", icon = "icons\\linkedin.svg", href = "https://www.linkedin.com/in/crjrg/"),
     GITHUB(text = "Cromega Github", icon = "icons\\github.svg", href = "https://github.com/Cromega08"),
     EMAIL(text = "cr.jrg08@gmail.com e-mail", icon = "icons\\email.svg", href = "mailto:cr.jrg08@gmail.com"),
-    CV(text = "Cristian CV", icon = "icons\\cv.svg", href = "documents\\cv_cjrg.pdf"),
+    CV(text = "Cristian CV", icon = "icons\\cv.svg", href = "documents\\cv_cjrg.zip"),
     CREDITS(text = "Resources Credits", icon = "icons\\credits.svg", href = "documents\\credits.md"),
     ENGLISH(text = "ENGLISH", icon = "icons\\english.svg", href = ""),
     SPANISH(text = "SPANISH", icon = "icons\\spanish.svg", href = "");
